505d34eb83e6e62b4b6b3f82193f1e183518c761
snadn <snadn@snadn.cn>
2016-12-21 10:36:53 +0800

    update

--- demo/view/home/app.js
+++ demo/view/home/app.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default class App extends React.Component {
 	render() {
-		console.log('app')
-		return <h1>Hello Think-React-App</h1>
+		return <app>
+			<h1>Hello Think-React-App</h1>
+			{this.props.children}
+		</app>
 	}
 }
--- demo/view/home/routes.js
+++ demo/view/home/routes.js
@@ -1,6 +1,13 @@
+import React from 'react'
 import App from './app'
 
 export default {
 	path: '/',
 	component: App,
+	childRoutes: [
+		{
+			path: 'test',
+			component: function() { return <p>test</p> }
+		}
+	]
 }
